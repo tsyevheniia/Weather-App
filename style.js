c43536a7b3f2c656651bf4975bd94550fe2fd815
Yevheniia Tsapar <ts.yevheniia@gmail.com>
2024-12-12 20:50:27 +0100

    modified version

--- style.js
+++ style.js
@@ -1,87 +1,94 @@
-const Api_Key = '7c5b7c16c8ec4f44a45105749243110'
-const toggleMode = document.getElementById('toggleMode');
-const textMode = document.getElementById('mode');
-const btn = document.getElementById('location-button');
+const apiKey = '7c5b7c16c8ec4f44a45105749243110'
+const textMode = document.getElementsByClassName('form-check-label')[0];
+const btnLocation = document.getElementById('btn-location');
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 const sections = document.getElementsByClassName('sections')[0];
 
+const exampleModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+
+const hideModal = ()=> {
+    if (exampleModal) {
+        exampleModal.dispose(); // Повністю видаляє інстанс модального вікна
+    }
+    document.getElementsByClassName('modal')[0].style.display = 'none';
+}
+
+btnLocation.addEventListener('click', async () => {
+    try {
+        btnLocation.classList.add('disabled');
+        exampleModal.show();
+        await getUserLocation();
+        await getWeatherByPosition();
+    } catch (error) {
+        alert('Error: ' + error);
+    }
+});
+
 let lat, lng;
 document.body.classList.add('lightMode');
 
-toggleMode.addEventListener('change', function () {
+
+const toggleDarkMode = ()=> {
     document.body.classList.toggle('lightMode');
     document.body.classList.toggle('darkMode');
-    if (document.body.classList.contains('lightMode')) {
-        btn.style.color = 'black';
-        textMode.textContent = 'To Dark Mode';
-        textMode.style.color = 'black';
-    } else {
-        btn.style.color = 'white';
-        textMode.textContent = 'To Light Mode';
-        textMode.style.color = 'white';
-    }
-})
 
-const locationButton = document.getElementById('location-button')
-locationButton.addEventListener('click', async function () {
-        await getUserLocation()
-        await getWeatherByPosition()
-        sections.classList.remove('hidden')
+    if (document.body.classList.contains('lightMode')) {
+        textMode.textContent = 'To Dark Mode';
+    } else {
+        textMode.textContent = 'To Light Mode';
     }
-)
-searchInput.addEventListener('keydown', async function (e) {
+}
+searchInput.addEventListener('keydown', async (e) => {
     if (e.key === 'Enter' && searchInput.value !== '') {
         e.preventDefault();
         await getWeatherByCity()
         sections.classList.remove('hidden')
         searchInput.value = '';
-
     }
 })
-searchButton.addEventListener('click', async function (e) {
+
+searchButton.addEventListener('click', async (e) => {
     e.preventDefault();
     await getWeatherByCity()
     sections.classList.remove('hidden')
     searchInput.value = '';
 })
 
-const getUserLocation = function () {
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(
-            function (position) {
-                lat = position.coords.latitude;
-                lng = position.coords.longitude;
-                resolve();
-            },
-            function (positionError) {
-                reject(positionError);
-            }
-        );
-    });
-};
-const errorMessage = function (){
-    alert("Wrong name of city! Try again!")
+const getUserLocation = () => new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+         (position) => {
+            lat = position.coords.latitude;
+            lng = position.coords.longitude;
+
+            resolve();
+        },
+         (positionError)=> {
+            reject(positionError);
+        }
+    );
+});
+const errorMessage = ()=> {
+    alert('Wrong name of city! Try again!')
 }
-const getWeatherByPosition = async function () {
-    await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${Api_Key}&q=${lat},${lng}&days=10&aqi=no&alerts=no`)
+const getWeatherByPosition = async () => {
+    const value = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${lat},${lng}&days=10&aqi=no&alerts=no`)
+    console.log(value.ok)
+    const response = await value.json()
+    console.log(response)
+    if (value.ok){
+        hideModal()
+        innerHtml(response)
+    }
+}
+const getWeatherByCity = async () => {
+    await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${searchInput.value}&days=10&aqi=no&alerts=no`)
         .then(value => value.json())
         .then(val => innerHtml(val))
         .catch(error => errorMessage())
-
-}
-const getWeatherByCity = async function () {
-    await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${Api_Key}&q=${searchInput.value}&days=10&aqi=no&alerts=no`)
-        .then(value => value.json())
-        .then(val => {
-            console.log(val);
-
-            innerHtml(val)
-        })
-        .catch(error => errorMessage())
 }
 
-const createWeatherDetails = function (id, iconSrc, value, label) {
+const createWeatherDetails = (id, iconSrc, value, label) => {
     return `
         <div id="${id}">
             <img src="${iconSrc}" alt="${label}">
@@ -90,7 +97,7 @@ const createWeatherDetails = function (id, iconSrc, value, label) {
         </div>
     `;
 }
-const createMainDetails = function (id, src, alt, secondId,label, value){
+const createMainDetails = (id, src, alt, secondId, label, value) => {
     return `<div id="${id}">
         <img src="${src}" alt="${alt}">
             <div id="${secondId}">
@@ -99,20 +106,20 @@ const createMainDetails = function (id, src, alt, secondId,label, value){
             </div>
     </div>`
 }
-const createForecastFor3Days = function (id,src,temp,date){
-    return `<div id="${id}">
+const createForecastFor3Days = (id, src, temp, date) => {
+     return `<div id="${id}">
         <img src="${src}">
             <p>${temp}</p>
             <p>${date}</p>
     </div>`
 }
 
-const innerHtml = function (value) {
+const innerHtml =  (value) => {
     sections.innerHTML = `
     <div class="section-up section1 container">
-            <h1>${value.location.name}</h1>
-            <h1>${value.location.localtime.split(" ")[1]}</h1>
-            <p>${value.location.localtime.split(" ")[0].split("-").reverse().join("-")}</p>
+        <h1>${value.location.name}</h1>
+        <h1>${value.location.localtime.split(' ')[1]}</h1>
+        <p>${value.location.localtime.split(' ')[0].split("-").reverse().join("-")}</p>
     </div>
     <div class="section-up section2 container">
         <div class="main-details">
@@ -133,40 +140,44 @@ const innerHtml = function (value) {
         </div>
     </div>
     <div class="section-down section3 container">
-        <h1>3 Days Forecast:</h1>
+        <h1>3 Days Forecast</h1>
         ${createForecastFor3Days('section3-row1', `${value.forecast.forecastday[0].day.condition.icon}`, `${value.forecast.forecastday[0].day.avgtemp_c}°C`, `${value.forecast.forecastday[0].date.split("-").reverse().join('.')}`)}
         ${createForecastFor3Days('section3-row2', `${value.forecast.forecastday[1].day.condition.icon}`, `${value.forecast.forecastday[1].day.avgtemp_c}°C`, `${value.forecast.forecastday[1].date.split("-").reverse().join('.')}`)}
         ${createForecastFor3Days('section3-row3', `${value.forecast.forecastday[2].day.condition.icon}`, `${value.forecast.forecastday[2].day.avgtemp_c}°C`, `${value.forecast.forecastday[2].date.split("-").reverse().join('.')}`)}
     </div>
      <div class="section-down section4 container">
-        <h1>Hourly Forecast:</h1>
+        <h1>Hourly Forecast</h1>
         <div class="section4-container">
         ${createHoursForecast(value)} 
         </div>
     </div>
     `
 }
-    const createHoursForecast = function (value) {
-        let dailyHours = [9, 12, 15, 18]
-        let forecastHtml = '';
-        dailyHours.forEach(hour => {
-            forecastHtml += `<div class="section4-column light">
+const createHoursForecast =  (value) => {
+    let dailyHours = [9, 12, 15, 18]
+    let forecastHtml = '';
+
+    dailyHours.forEach(hour => {
+        forecastHtml +=
+            `<div class="section4-column light">
+                    <p>${hour}:00</p>
+                    <img src=${value.forecast.forecastday[0].hour[hour].condition.icon} alt="sun">
+                        <p>${value.forecast.forecastday[0].hour[hour].temp_c}</p>
+                        <i class="fa-solid fa-wind"></i>
+                        <p>${value.forecast.forecastday[0].hour[hour].wind_kph}km/h</p>
+                </div>`
+    })
+
+    let nightHours = [21, 0]
+
+    nightHours.forEach(hour => {
+        forecastHtml += `<div class="section4-column dark">
             <p>${hour}:00</p>
             <img src=${value.forecast.forecastday[0].hour[hour].condition.icon} alt="sun">
                 <p>${value.forecast.forecastday[0].hour[hour].temp_c}</p>
                 <i class="fa-solid fa-wind"></i>
                 <p>${value.forecast.forecastday[0].hour[hour].wind_kph}km/h</p>
         </div>`
-        })
-        let nightHours = [21, 0]
-        nightHours.forEach(hour => {
-            forecastHtml += `<div class="section4-column dark">
-            <p>${hour}:00</p>
-            <img src=${value.forecast.forecastday[0].hour[hour].condition.icon} alt="sun">
-                <p>${value.forecast.forecastday[0].hour[hour].temp_c}</p>
-                <i class="fa-solid fa-wind"></i>
-                <p>${value.forecast.forecastday[0].hour[hour].wind_kph}km/h</p>
-        </div>`
-        })
-        return forecastHtml;
-    }
+    })
+    return forecastHtml;
+}
